c1e37123adad4110471191e6268f90ea96c5eb27
Bhawna Mulchandani <bhawna.bm@gmail.com>
2021-06-11 15:20:32 +0000

    Added flask route for a volunteer , beneficiary and a login page

--- server.py
+++ server.py
@@ -12,6 +12,32 @@ app.secret_key
 app.jinja_env.undefined = StrictUndefined
 
 
+@app.route('/')
+def homepage():
+    """Homepage for the good-samritan-finder app. """
+
+    return render_template('homepage.html')
+
+
+@app.route('/be-a-volunteer')
+def onboard_volunteer():
+    """Sign Up to be a Volunteer. """    
+
+    return render_template('onboard_volunteer.html')
+
+
+@app.route('/be-a-beneficiary')
+def onboard_beneficiary():
+    """Sign Up to be a Beneficiary. """    
+
+    return render_template('onboard_beneficiary.html')
+
+
+@app.route('/login', methods=['POST'])
+def user_login():
+    """Process the login of a user. """
+
+    return render_template('placeholder.html')
 
 if __name__ == "__main__":
     # Connecting to DB before running the app
