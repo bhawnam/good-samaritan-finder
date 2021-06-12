a38c3a8efe033f595a35824d024302f2004e93b2
Bhawna Mulchandani <bhawna.bm@gmail.com>
2021-06-12 20:52:40 +0000

    Added code to create DB entries for services table

--- seed_database.py
+++ seed_database.py
@@ -2,6 +2,7 @@
 
 import os
 import json 
+from random import randint
 
 import crud
 import model
@@ -14,9 +15,12 @@ os.system('createdb samaritan-finder')
 model.connect_to_db(server.app)
 model.db.create_all()
 
-# Creating 25 random user profiles to be stored in the database
+# Creating 25 random user profiles to be stored in the database for users table
 user_data = data_generation.create_user_profile()
 users_in_db = []
+   
+beneficiaries_in_db = []
+volunteers_in_db = []
 
 for user_key in user_data.keys():
     first_name = user_data[user_key]['first_name']
@@ -38,12 +42,43 @@ for user_key in user_data.keys():
                                 phone_number)    
 
     users_in_db.append(user)  
-    # Creating beneficiary entries for the database
+
+     # Creating beneficiary entries in beneficiaries table in the database
     if (user_key % 2 == 0):
         is_b_onboarded = True
         beneficiary = crud.create_beneficiary(is_b_onboarded, user)
-    
-    # Creating volunteer entries for the database
+        beneficiaries_in_db.append(beneficiary)
+    # Creating volunteer entries volunteers table in the database
     else:
         is_v_onboarded = True
         volunteer = crud.create_volunteer(is_v_onboarded, user)  
+        volunteers_in_db.append(volunteer)
+
+# Creating service type entries services table in the database
+service_type_db = []
+service_name = ["PACKAGED_MEAL_KIT", "WATER", "FIRST_AID_KIT", "BLANKET", "PET_FOOD"]
+
+for name in service_name:
+    for_num_persons = randint(1,10)
+    service_type = crud.create_service_type(name, for_num_persons)
+    service_type_db.append(service_type)
+
+# service_offered_in_db = []
+
+# for beneficiary in beneficiaries_in_db:
+#     rating = randint(1,5)
+#     request = ''
+#     beneficiary_rating = crud.create_beneficiary_rating(rating, beneficiary, request)
+
+
+# for volunteer in volunteers_in_db:
+#     rating = randint(1,5)
+#     response = ''
+#     volunteer_rating = crud.create_volunteer_rating(rating, volunteer, response)
+
+
+
+
+
+
+
