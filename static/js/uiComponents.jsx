bb139aed1c91ec366edd62d906ca65bbdff0590e
Bhawna Mulchandani <bhawna.bm@gmail.com>
2021-07-17 23:56:25 +0000

    Fixed the disabled button issue to disable the button only when its clicked and not all the buttons

--- static/js/uiComponents.jsx
+++ static/js/uiComponents.jsx
@@ -456,10 +456,7 @@ function UserProfile(props){
       service_type={currentFulfilledRequest.service_type}
       for_num_persons={currentFulfilledRequest.for_num_persons}
       handleRequestFeedbackBtn={handleFeedbackButton}
-      disableBtn={disable}
-      // feedbackRequestForm={feedbackForm}
-      // handleFeedbackResponseForm={handleFeedbackResponse}
-      // feedbackMessage={feedbackMessage}
+      feedbackRequestid={feedbackRequestID}
       />
       );
       fulfilledRequestsTableData.push(requestFeedbackCard);
@@ -747,14 +744,14 @@ function RequestAccept(props){
 
 function RequestFeedback(props){
 
-  const {request_id, handleRequestFeedbackBtn, service_type, disableBtn} = props;
-  // const {request_id, handleRequestFeedbackBtn, service_type, disableBtn, feedbackRequestForm, handleFeedbackResponseForm, feedbackMessage} = props;
+  const {request_id, handleRequestFeedbackBtn, service_type, feedbackRequestid} = props;
+
   return (
     <React.Fragment>
     <tr key = {request_id}>
     <td>{request_id}</td>
     <td>{service_type} </td>
-    <td> <button type="submit" className="btn" disabled={disableBtn} onClick={()=>handleRequestFeedbackBtn(request_id)}> Give Feedback </button>
+    <td> <button type="submit" className="btn" disabled={feedbackRequestid === request_id} onClick={()=>handleRequestFeedbackBtn(request_id)}> Give Feedback </button>
     </td>
   </tr>
   </React.Fragment>
