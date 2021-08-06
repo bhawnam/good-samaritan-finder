fbc8c64d9935fb3820cf3156054fdf97c631c9ff
Bhawna Mulchandani <bhawna.bm@gmail.com>
2021-08-06 14:18:25 -0700

    Added an initial version of sidebar with quick links.

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Button } from "react-bootstrap";
 
 export default function SideBar() {
-  return (
-    <div>
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+//   return (
+//     <div>
+//       <div className="sidebar">
+//         <div className="side-item">
+//           <Link to="/requests" className="side-link">
+//             <b> Requests </b>
+//             <br />
+//           </Link>
+//           <Link to="/offerings" className="side-link">
+//             <b> Offerings </b>
+//             <br />
+//           </Link>
+//           <Link to="/matched-requests" className="side-link">
+//             <b> Matched Requests </b>
+//             <br />
+//           </Link>
+//           <Link to="/fulfilled-requests" className="side-link">
+//             <b> Fulfilled Requests </b>
+//             <br />
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+return (
+  <>
+  <br/>
+  <br/>
+  <br/>
+    <Button variant="primary" onClick={handleShow}>
+      Quick Links
+    </Button>
+
+    <Offcanvas show={show} onHide={handleClose}>
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Quick Links</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
       <div className="sidebar">
         <div className="side-item">
           <Link to="/requests" className="side-link">
@@ -24,6 +67,8 @@ export default function SideBar() {
           </Link>
         </div>
       </div>
-    </div>
-  );
+      </Offcanvas.Body>
+    </Offcanvas>
+  </>
+);
 }
